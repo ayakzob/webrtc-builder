e895c2899aa0ae2efe0e36e02e844ef49de817be
Ugur Bozkaya <eminbozkaya@gmail.com>
2016-12-25 18:14:50 -0500

    Update build.sh
    Install missing libraries if necessary

--- build.sh
+++ build.sh
@@ -1,6 +1,53 @@
+#UPDATE REPOS
+sudo yum check-update
+sudo apt-get update
+
 #INSTALL GIT
 sudo apt-get -y install git 
 sudo yum -y install git
+
+LINUX_ID=`cat /etc/os-release | egrep -i -o "^ID=(.*)" | egrep -io "[a-z]*" | grep -v ID`
+LINUX_ID_LIKE=`cat /etc/os-release | egrep -i -o "^ID_LIKE=(.*)" | egrep -io "[a-z]*" | egrep -v '(ID|LIKE)'`
+LINUX_VERSION_ID=`cat /etc/os-release | egrep -i -o "^VERSION_ID=(.*)" | egrep -io "[0-9\.]*"`
+echo "LINUX_ID=$LINUX_ID"
+echo "LINUX_ID_LIKE=$LINUX_ID_LIKE"
+echo "LINUX_VERSION_ID=$LINUX_VERSION_ID"
+
+#Install lbzip2, RED HAT seems to need this
+#http://lbzip2.org/download
+if which lbzip2 >/dev/null; then
+	echo "lbzip2 exists (http://lbzip2.org/download)"
+else
+	if [ "$LINUX_ID" = "rhel" ]; then 
+		echo "Installing lbzip2 (http://lbzip2.org/download)"
+		sudo yum -y  groupinstall 'Development Tools'
+		curl -O http://archive.lbzip2.org/lbzip2-2.5.tar.gz
+		tar -xvzf lbzip2-2.5.tar.gz
+		cd lbzip2-2.5
+		./configure
+		make check
+		sudo make install
+		cd ..
+	else
+		echo "Missing lbzip2 (http://lbzip2.org/download)"
+	fi;
+fi
+
+
+#install gtk2.0, UBUNTU seems to need this
+if [ "$LINUX_ID" = "ubuntu" ]; then 
+	sudo apt-get install -y gtk2.0
+
+	#Install python27, Ubuntu 16 seems to need this
+	if which python2.7 >/dev/null; then
+		echo "python2.7 exists"
+	else
+		echo "Installing python 2.7"
+		sudo apt-get install -y python-minimal
+	fi
+fi;
+
+
 
 #INSTALL DEPOT TOOLS
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
@@ -24,12 +71,13 @@ git checkout master
 #To generate ninja project files for a Release build instead:
 
 #DEFAULT DEBUG BUILD
-#to generate qt creator project files use this command instead
-#gn gen --ide ="qtcreator" out/Debug 
-gn gen out/Debug
+#gn gen out/Debug
+#generate optional qt creator project files
+gn gen --ide ="qtcreator" out/Debug 
 
 #OPTIONAL RELEASE BUILD
 #gn gen out/Release --args='is_debug=false'
+#gn gen --ide ="qtcreator" out/Release --args='is_debug=false'
 
 #THIS IS THE COMMAND TO CLEAN THE BUILD
 #To clean all build artifacts in a directory but leave the current GN configuration untouched (stored in the args.gn file), do:
